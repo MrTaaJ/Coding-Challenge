81babdf2400dc2b02ec2e1f0b69d74c408719348
Saheed Tajudeen <saheed.taaj@gmail.com>
2021-10-16 08:09:51 +0100

    added data-type.js

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-[Coding ]
+# HackerRank Coding Challenges
+
+1.  [Data Types (Difficulty -Easy) Solution on HackerRank](https://www.hackerrank.com/challenges/js10-data-types/problem)
+    [JavaScript File Solution]()
---
+++ easy-js/data-type.js
@@ -0,0 +1,10 @@
+function performOperation(secondInteger, secondDecimal, secondString) {
+    const firstInteger = 4;
+    const firstDecimal = 4.0;
+    const firstString = 'HackerRank ';
+    console.log (parseInt(firstInteger) + parseInt(secondInteger))
+    console.log (parseFloat(firstDecimal) + parseFloat(secondDecimal))
+    console.log (firstString + secondString)
+}
+
+//This code will run on HackerRank website because it has some inbuilt functions on their website.
